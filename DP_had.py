fe9b8e2185061d3c4eada336753678f05ee8aad9
Jana Fedrová <jana@fedra.cz>
2016-11-27 14:57:08 +0100

    DP_had
    pohyb pomoci klaves wsad bez mackani klavesy enter

--- DP_had.py
+++ DP_had.py
@@ -1,9 +1,9 @@
+"""ovládání pomocí kláves wsad bez entru"""
+
+import msvcrt
 from random import randrange
 
 def nakresli_mapu(souradnice, ovoce, delka_pole, vyska_pole):
-    """Vykreslí hrací pole, kde na zadaných souřadnicích bude had a na další
-       ovoce pro hada. Zbytek pole budou tečky.
-    """
     print(20*"-")
     for radek in range(0, vyska_pole):
         for sloupec in range(0, delka_pole):
@@ -17,20 +17,17 @@ def nakresli_mapu(souradnice, ovoce, delka_pole, vyska_pole):
     if delka_pole < 4 or vyska_pole < 1:
         raise ValueError("Nastav hrací pole.")
 
-def pohyb(souradnice, strana, delka_pole, vyska_pole):
-    """S hadem se hýbe pomocí kláves swda. Pokud had vyjede z pole nebo narazí
-       do sebe, hra se ukončí. Když had sní ovoce, objeví se na náhodném místě
-       nové. Taky se objeví nové ovoce po každých třiceti tazích.
-    """
+def pohyb(souradnice, delka_pole, vyska_pole):
     posledni_bod = souradnice[-1]
     x, y = posledni_bod
-    if strana == "s":
+    key = msvcrt.getch().decode("utf-8").lower()
+    if key == "s":
         novy_bod = x, y + 1
-    elif strana == "w":
+    elif key == "w":
         novy_bod = x, y - 1
-    elif strana == "d":
+    elif key == "d":
         novy_bod = x + 1, y
-    elif strana == "a":
+    elif key == "a":
         novy_bod = x - 1, y
     else:
         raise ValueError(strana)
@@ -49,9 +46,6 @@ def pohyb(souradnice, strana, delka_pole, vyska_pole):
         pridej_ovoce(souradnice, ovoce, delka_pole, vyska_pole)
 
 def pridej_ovoce(souradnice, ovoce, delka_pole, vyska_pole):
-    """Dokud jsou v poli volná místa (tečky), tak na náhodném místě vyroste
-       nové ovoce a přidá se do seznamu ovoce.
-    """
     velikost_pole = delka_pole * vyska_pole
     velikost_ovoce = len(ovoce)
     velikost_souradnice = len(souradnice)
@@ -63,17 +57,19 @@ def pridej_ovoce(souradnice, ovoce, delka_pole, vyska_pole):
             nove_ovoce = x, y
         ovoce.append(nove_ovoce)
 
+
+print()
+print("Hra se ovládá pomocí kláves wsad")
 souradnice = [(0, 0), (1, 0), (2, 0)]
 ovoce = [(2, 3)]
 pocet = 0
-delka_pole = 10
-vyska_pole = 10
+delka_pole = 8
+vyska_pole = 8
 while True:
     pocet = pocet + 1
     nakresli_mapu(souradnice, ovoce, delka_pole, vyska_pole)
-    strana = input("Zadej stranu (w, s, a, d): ")
-    pohyb(souradnice, strana, delka_pole, vyska_pole)
-    print (souradnice)
+    #strana = input("Zadej stranu (w, s, a, d): ")
+    pohyb(souradnice, delka_pole, vyska_pole)
     if (delka_pole * vyska_pole) == (len(souradnice)):
         nakresli_mapu(souradnice, ovoce, delka_pole, vyska_pole)
         print("Vyhrál jsi.")
